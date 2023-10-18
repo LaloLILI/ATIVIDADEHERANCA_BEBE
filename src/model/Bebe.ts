97ba482e9e4e829810c1dcf3edabc71c864a7458
Helli Miranda <larissamiranda225@gmail.com>
2023-10-18 09:15:04 -0300

    new commit
    aa

---
+++ src/model/Bebe.ts
@@ -0,0 +1,45 @@
+export abstract class Bebe {
+    private id: string;
+    private nome: string;
+    private peso: number;
+    private altura: number;
+
+    public constructor(_id: string, _nome: string, _peso: number, _altura: number) {
+        this.id = _id;
+        this.nome = _nome;
+        this.peso = _peso;
+        this.altura = _altura;
+    }
+
+    public setID(_id: string): void {
+        this.id = _id;
+    }
+
+    public getID(): string {
+        return this.id;
+    }
+
+    public setNome(_nome: string): void {
+        this.nome = _nome;
+    }
+
+    public getNome(): string {
+        return this.nome;
+    }
+
+    public setPeso(_peso: number): void {
+        this.peso = _peso;
+    }
+
+    public getPeso(): number {
+        return this.peso;
+    }
+
+    public setAltura(_altura: number): void {
+        this.altura = _altura;
+    }
+
+    public getAltura(): number {
+        return this.altura;
+    }
+}
